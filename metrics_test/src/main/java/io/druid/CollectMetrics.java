4961e2379ce2f57b600310be23dbf0646f2a45aa
liangrenhua <liangrenhua92nld@gmail.com>
2017-11-02 14:43:13 +0800

    change opentsdb host to 10.19.138.167

--- metrics_test/src/main/java/io/druid/CollectMetrics.java
+++ metrics_test/src/main/java/io/druid/CollectMetrics.java
@@ -43,7 +43,7 @@ public class CollectMetrics {
                                 EnnMetricsConfig.getConfigWithFreq(1, "metricsTest")),
                         new OpentsdbHttpReporterModule(
                                 OpentsdbConfig.newBuilder()
-                                        .setHostname("http://monitoring-opentsdb.monitor-essential-service")
+                                        .setHostname("http://10.19.138.167")
                                         .setPort(4242)
                                         .build()));
 
